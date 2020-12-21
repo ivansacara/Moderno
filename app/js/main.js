4f36ffd3efbd71c0cf658077be0a99ec9f5313a6
ivansacara <ivansacara@gmail.com>
2020-12-21 17:36:55 +0200

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+	$(".rate-star").rateYo({
+		rating: 5,
+		readOnly: true,
+		starWidth: "12px"
+	});
+
+	$('.product-slider-inner').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+
+
+	});
+
 	var mixer = mixitup('.products__inner-box');
 
 
