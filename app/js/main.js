f9f6cd9edea38e956ab4542527823a296d621fca
ivansacara <ivansacara@gmail.com>
2021-01-08 15:45:00 +0200

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+// const { on } = require('gulp');
+
 $(function () {
 
 	$(".rate-star").rateYo({
@@ -14,6 +16,27 @@ $(function () {
 
 
 	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+	$('.icon-th-list').on('click', function () {
+		$('.page').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.icon-th-large').removeClass('active');
+	});
+	$('.icon-th-large').on('click', function () {
+		$('.page').removeClass('list');
+		$('.icon-th-large').addClass('active');
+		$('.icon-th-list').removeClass('active');
+	});
+
 
 	var mixer = mixitup('.products__inner-box');
 
