454a25b4b5594516e412707eb157aacb411ddb22
ivansacara <ivansacara@gmail.com>
2021-01-20 15:43:10 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,8 +12,36 @@ $(function () {
 		dots: true,
 		arrows: false,
 		slidesToShow: 4,
-		slidesToScroll: 4
-
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1900,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 1150,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
+			},
+		]
 
 	});
 
